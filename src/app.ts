3b605845e14e9727f21886c079fd8928ce0d73a1
asheque33 <ashequrrahman3333@gmail.com>
2023-12-28 13:06:04 +0600

    project setup successfully

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Parsers
+app.use(express.json());
+app.use(cors());
+
+//application routes
+// app.use('/api/users', userRouter);
+
+const getAController = (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to the Localhost API!',
+  });
+};
+app.get('/', getAController);
+
+console.log(process.cwd());
+
+export default app;
